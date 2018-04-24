8a40fb5c100873e5ba816242d69d49df61602437
christopher-koleda <koleda.christopher@gmail.com>
2018-04-24 04:17:18 -0400

    Add post comment box no logic

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,20 +3,35 @@
 @section('content')
   <div class="u-wrapper">
     <div class="post-detail">
-      <h1 class='post-detail title is-1'>
+      <h1 class='post-detail__title title is-1 is-marginless'>
         {{$post->title}}
       </h1>
-      <div class='post-detail body'>
+      <div class='post-detail__body'>
         {{$post->body}}
       </div>
-      <p class='post-detail author is-size-5'>
+      <p class='post-detail__author is-size-5'>
         -{{$post->author}}
       </p>
-      @foreach ($comments as $comment)
-        <div class='post-detail comments'>
-          @include('partials.comments')
+      <form class="post-detail__form">
+          {{ csrf_field() }}
+        <div class="field">
+          <div class="control">
+            <textarea class="textarea" 
+            placeholder="Post a comment" 
+            rows="5"></textarea>
+          </div>
         </div>
-      @endforeach
+        <div class="field">
+          <div class="control">
+              <button class="button is-primary">Submit</button>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
+  @foreach ($comments as $comment)
+    <div class='comments'>
+      @include('partials.comments')
+    </div>
+  @endforeach
 @endsection
