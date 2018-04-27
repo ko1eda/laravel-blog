7aec4a02fe0cd74a30dc810369f534fef2f33f70
christopher-koleda <koleda.christopher@gmail.com>
2018-04-27 00:07:30 -0400

    Add mock stripe service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Billing\Stripe;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,11 +30,28 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Register any application services.
+     * note we use singleton here instead of bind because
+     * we are registerting it as a singleton:
+     *  This means that whenever the service is refrenced it will
+     *  use the same instance instead of making a new instance each time
+     *  this is useful when providing a service that requires an api key obivously.
      *
+     * what this does is register your class or interface with laravels IoC container
+     * which allow it to then resolve your class automatically when it is required as a typehinted
+     * dependency. It will resolve it to whatever you provide it inside the function closure
+     *
+     * You can then register your Service provider files inside the config.app.php file as an alias.
+     * In general I would assume you would probably make spereate files for your service providers
+     *
+     * If you have a service provider that does not need to use its boot method
+     * you can add a protected $defer = true property to it
+     * this means that it will only load the service when it is necessary
      * @return void
      */
     public function register()
     {
-        //
+        \App::singleton(Stripe::class, function () {
+            return new Stripe(config('services.stripe.secret'));
+        });
     }
 }
